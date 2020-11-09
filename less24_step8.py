a7ab30753d72bd738e211da6ac9e1b1dfe3279cd
vagrant <vagrant@u2004dw>
2020-11-09 11:32:05 +0300

    add some files

---
+++ less24_step8.py
@@ -0,0 +1,41 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time 
+import math
+from selenium.webdriver.support.ui import Select
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/explicit_wait2.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+    button = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#price"),'$100'))
+
+    button = browser.find_element_by_css_selector("#book")
+    button.click()
+
+    x = int(browser.find_element_by_css_selector("#input_value").text)
+
+    inputy = browser.find_element_by_css_selector("#answer")
+    y = calc(x)
+    inputy.send_keys(y)
+
+    button = browser.find_element_by_css_selector("[type='submit']")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
